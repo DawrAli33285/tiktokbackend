cefb7522539b7aa11e65c6f68473ca2269739696
DawrAli33285 <lemightyeagle@gmail.com>
2026-04-09 14:39:35 +0500

    new

--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-let connect=mongoose.connect('mongodb://127.0.0.1/trending')
+let connect=mongoose.connect('mongodb+srv://dawar:dawar@cluster0.7b9t5dx.mongodb.net')
 
 
 module.exports=connect
